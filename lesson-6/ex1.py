2f3f56bbc3054e21be9cec51604f68bc6a81bff5
Claudio Brandolino <cbrandolino@gmail.com>
2015-12-17 19:27:57 +0100

    Added exercise 6.1

---
+++ lesson-6/ex1.py
@@ -0,0 +1,7 @@
+# Age/Net Worth Regression in sklearn
+
+def studentReg(ages_train, net_worths_train):
+    from sklearn.linear_model import LinearRegression
+    reg = LinearRegression()
+    reg.fit(ages_train, net_worths_train)
+    return reg
