864ff47e1f089972874d0717bd2f4481f628a36c
Claudio Brandolino <cbrandolino@gmail.com>
2015-12-24 06:00:57 +0100

    Added exercise 7.3-4

--- lesson-7/outlier_cleaner.py
+++ lesson-7/outlier_cleaner.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 def outlierCleaner(predictions, ages, net_worths):
     """
         Clean away the 10% of points that have the largest
@@ -10,11 +9,11 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
-
-    ### your code goes here
-
+    tuples_list = map(lambda x: (x[1][0], x[2][0], abs(x[0][0] - x[2][0])), zip(predictions, ages, net_worths))
+    tuples_list.sort(lambda x, y: cmp(x[2], y[2]))
+    cleaned_data = tuples_list[:81]
     
     return cleaned_data
 
